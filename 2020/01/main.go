6ef2bc40164b2a0e3251bf6e25719681ef6f5f7f
Tyler Suggs <ty@tysug.gs>
2020-12-23 22:07:31 -0500

    2020 day 1 part 2

--- 2020/01/main.go
+++ 2020/01/main.go
@@ -6,13 +6,25 @@ const target = 2020
 
 func main() {
 
-	result := twoSum(input, target)
+	p1result := twoSum(input, target)
 
-	one := input[result[0]]
-	two := input[result[1]]
+	p1one := input[p1result[0]]
+	p1two := input[p1result[1]]
 
-	fmt.Printf("%v + %v = 2020\n", one, two)
-	fmt.Printf("%v * %v = %v", one, two, one*two)
+	fmt.Println("Part 1:")
+	fmt.Printf("%v + %v = 2020\n", p1one, p1two)
+	fmt.Printf("%v * %v = %v\n\n", p1one, p1two, p1one*p1two)
+
+	p2result := threeSum(input, target)
+
+	p2one := input[p2result[0]]
+	p2two := input[p2result[1]]
+	p2three := input[p2result[2]]
+
+	fmt.Println("Part 2:")
+	fmt.Printf("%v + %v + %v = 2020\n", p2one, p2two, p2three)
+	fmt.Printf("%v * %v * %v = %v\n", p2one, p2two, p2three, p2one*p2two*p2three)
+
 }
 
 func twoSum(nums []int, target int) []int {
@@ -22,6 +34,18 @@ func twoSum(nums []int, target int) []int {
 			return []int{idx, k}
 		}
 		m[v] = k
+	}
+	return nil
+}
+
+func threeSum(nums []int, target int) []int {
+	for k, v := range nums {
+		t := target - v
+		ts := twoSum(input, t)
+
+		if ts != nil {
+			return []int{ts[0], ts[1], k}
+		}
 	}
 	return nil
 }
